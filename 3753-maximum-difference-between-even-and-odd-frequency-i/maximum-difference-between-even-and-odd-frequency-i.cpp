e2580f50f38f509321a8875fd47566ee77c2de0b
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2025-06-12 01:25:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (60.43%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        unordered_map<char,int>mp;
-        int maxv=INT_MIN;
-        int minv=INT_MAX;
-
-        for(auto i: s){
-            mp[i]++;
-        }
-        for(auto i:mp){
-            if(i.second%2==0){
-                minv=min(minv,i.second);
-            }
-            else{
-                maxv=max(maxv,i.second);
-            }
-        }
-
-        return maxv-minv;
+        vector<int> mpp(26);
+        int maxi = 0, mini = s.size();
+              for (char c : s) mpp[c - 'a']++;
+        for (int i = 0 ; i < 26; i++) {
+            if (mpp[i] % 2 != 0) maxi = max(maxi, mpp[i]);
+            if (mpp[i] % 2 == 0 && mpp[i] > 0) mini = min(mini, mpp[i]);
+        } 
+        return maxi - mini;
     }
 };
