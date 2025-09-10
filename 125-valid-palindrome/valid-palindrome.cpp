fc69b1be60eb5cf1369a8b98ba056f1d489dffe3
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2025-09-10 22:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (42.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n=s.length();
         string ans="";
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.length();i++){
             if(isalnum(s[i])){
                 ans.push_back(tolower(s[i]));
             }
         }
         s=ans;
         reverse(s.begin(),s.end());
-        return ans==s;
+        if(s == ans){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
