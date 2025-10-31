d925d351b04d645b099e1dd7ce99fd238f3c593f
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2025-11-01 00:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.56%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,6 +1,5 @@
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
+    int searchbymymethod(vector<int>& nums, int target) {
         int n=nums.size();
         int i=0;
         int x=n-2;
@@ -39,5 +38,49 @@ public:
             else e=mid-1;
         }
         return -1;
+    }
+//  
+    int youtubeMethod(vector<int>&nums,int target){
+        int s=0,e=nums.size()-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]>nums[e]){
+                s=mid+1;
+            }
+            else {
+                e=mid;
+            }
+        }
+        int p=s;
+        s=0;
+        e=nums.size()-1;
+        if (target>=nums[p] && target<=nums[e]){
+            s=p; 
+        }
+        else {
+            e=p-1;
+        }
+
+
+        while(s<=e){
+                int mid=s+(e-s)/2;
+                if(nums[mid]==target){
+                    return mid;
+                }
+                else if(nums[mid]<target){
+                    s=mid+1;
+                }
+                else e=mid-1;
+        }
+
+
+
+        return -1;
+    }
+
+public:
+    int search(vector<int>& nums, int target) {
+        //return searchbymymethod(nums,target);
+        return youtubeMethod(nums,target);
     }
 };
