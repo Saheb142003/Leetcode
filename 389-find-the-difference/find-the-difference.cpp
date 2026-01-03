7788ebe493f49d141ce669f620aeb4e2f294b8a0
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2026-01-04 04:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (76.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sum=0;
-        for(auto i:t){
-            sum+=(i-'a');
-        }
-        for(auto i:s){
-            sum-=(i-'a');
-        }
-        return (sum+'a');
+       int sum=0;
+       for(auto i:t){
+        sum+=(i-'a');
+       }
+       for(auto i:s){
+        sum-=(i-'a');
+       }
+       
+       return (sum+'a');
     }
 };
