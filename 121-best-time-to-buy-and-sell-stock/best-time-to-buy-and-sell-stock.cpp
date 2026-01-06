f0d9bf3f489ec6d1a36d29af78468233bb7f2984
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2026-01-07 01:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (59.83%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int buy=prices[0];int sell=0;
         for(int i=0;i<prices.size();i++){
             if(prices[i]<buy) buy=prices[i];
-            else sell=max(sell,(prices[i]-buy));
+            sell=max(sell,(prices[i]-buy));
         }
         return sell;
     }
