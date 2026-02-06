a89f564f8ee0009afdd5d2286b62dbcbf9bf5ac3
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2026-02-06 23:11:29 +0530

    Time: 28 ms (79.65%) | Memory: 105.1 MB (52.91%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -10,8 +10,7 @@ public:
         // 1 2 3 4 6 8 10 15 16 20 
         // 1 2 6 4
         while(j<n){
-            
-            if((long long)nums[i]*k>=(long long)nums[j] ){
+            if(((long long) nums[i]*k) >=(long long) nums[j] ){
                 ans=max(ans,j-i+1);
                 j++;
             }
@@ -19,7 +18,7 @@ public:
                 i++;
             }
         }
-        return (n-ans);
+        return n-ans;
     }
 };
 //18 22 58 68
