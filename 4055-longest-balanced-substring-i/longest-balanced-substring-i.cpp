2ed94558b8b8087e80e03355eed92a10ff222598
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2026-02-14 01:07:35 +0530

    Time: 31 ms (93.01%) | Memory: 11.4 MB (83.40%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -5,16 +5,17 @@ public:
         int ans=0;
         for(int i=0;i<n;i++){
             int freq[26]={0};
+            int high=0;
+            int low=0;
+            int dist=0;
             for(int j=i;j<n;j++){
-                freq[s[j]-'a']++;
-                int target=freq[s[j]-'a'];
-                int flag=true;
-                for(int k=0;k<26;k++){
-                    if(freq[k]!=target and freq[k]!=0){
-                        flag=false;
-                    }
-                }
-                if(flag) ans=max(ans,j-i+1);
+                char c=s[j]-'a';
+                if(freq[c]==0) dist++;
+                freq[c]++;
+                high=max(high,freq[c]);
+                low=max(low,freq[c]);
+                int len=j-i+1;
+                if(high == low and len==dist*high) ans=max(ans,j-i+1);
             }
         }
         return ans;
