fbcc522a75a2c72a6216ebdebda27bb47b229236
Md Sahebuddin Ansari <119444935+Saheb142003@users.noreply.github.com>
2026-06-18 17:44:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.08%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -3,9 +3,6 @@ public:
     double angleClock(int hour, int minutes) {
        
         double h = fmod((30 * (hour % 12)) + (minutes * 0.5), 360.0);
-        double m=6*minutes;        
-        
-
-        return abs(h-m)>180?(360-abs(h-m)):(abs(h-m));
+        return abs(h-6*minutes)>180?(360-abs(h-6*minutes)):(abs(h-6*minutes));
     }
 };
